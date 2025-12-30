a00ea748dec94a0d298bc21fd53a336f451020ef
Daniel Müller <deso@posteo.net>
2025-12-29 22:22:44 -0800

    Remove libsamplerate-rs re-export
    Remove the re-export of the libsamplerate-rs dependency. We treat it
    entirely as an implementation detail and see no point in giving users
    access to its functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@ mod resampler;
 #[cfg(test)]
 mod sanity_test;
 
-pub use libsamplerate_rs;
-
 pub use crate::error::Error;
 pub use crate::error::ErrorKind;
 pub use crate::resample_type::ResampleType;
